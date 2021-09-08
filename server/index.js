f0f63306c722e1a6b3765584710791a4da5b7270
Michael Glenn <dmichaelglenn@gmail.com>
2021-09-08 11:22:03 -0700

    color schema in; can get list of colors and post a color

--- server/index.js
+++ server/index.js
@@ -7,10 +7,40 @@ const port = process.env.PORT || 8081;
 
 mongoose.connect(process.env.MONGODB_URI);
 
+const ColorSchema = mongoose.Schema({
+    h: Number,
+    s: Number,
+    v: Number
+});
+
+const Color = mongoose.model("Color", ColorSchema);
+
 const app = express();
 
 app.use(express.static(__dirname + "/../public"));
 app.use(bodyParser.json());
+
+function listResources(Model) {
+    return function (req, res, next) {
+        Model.find(function(err, docs) {
+            if (err) return next(err);
+            res.json(docs);
+        })
+    }
+}
+
+function createResource(Model) {
+    return function (req, res, next) {
+        let resource = new Model(req.body);
+        resource.save(function (err, resource) {
+            if (err) return next(err);
+            res.json(resource);
+        });
+    };
+}
+
+app.get("/colors", listResources(Color));
+app.post("/colors", createResource(Color));
 
 const server = app.listen(port, function () {
     console.log("now running on port", port);
