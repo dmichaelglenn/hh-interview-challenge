331512d8bca9b6513129b76ed4b074ed0b0d28b1
Michael Glenn <dmichaelglenn@gmail.com>
2021-09-08 11:07:31 -0700

    server runs

--- server/index.js
+++ server/index.js
@@ -1,2 +1,17 @@
-require('dotenv').load({ silent: true });
+require('dotenv').config();
 
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const port = process.env.PORT || 8081;
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const app = express();
+
+app.use(express.static(__dirname + "/../public"));
+app.use(bodyParser.json());
+
+const server = app.listen(port, function () {
+    console.log("now running on port", port);
+})
