d320a9d17ac992b8a2264d1b1e989f500e137411
Michael Glenn <dmichaelglenn@gmail.com>
2021-09-13 16:04:00 -0700

    a few more adjustments for production deploy

--- public/src/scripts/variables.js
+++ public/src/scripts/variables.js
@@ -1,4 +1,4 @@
-const baseColorsUrl = 'http://localhost:8080/colors';
+const baseColorsUrl = '/colors';
 const perPageCount = 12;
 const mainWindow = document.getElementById('main');
 const activeWindow = document.getElementById('active-window');
